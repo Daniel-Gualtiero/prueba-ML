80f290d167ab8e8f5b9fa72cdeabd5406505f7fa
Daniel-Gualtiero <d_gualtiero@hotmail.com>
2021-06-29 00:14:51 -0500

    agregando archivos propios de ML

---
+++ app.js
@@ -0,0 +1,28 @@
+//Daniel-Gualtiero
+
+//Proyecto Mercado Libre
+
+//Require express
+const express = require('express');
+
+const app = express();
+
+//Require path
+const path = require('path');
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+//montar servidor
+app.listen(3020, () => console.log('Servidor corriendo en el puerto 3020'));
+
+app.get('/', (req, res) => {
+    res.sendFile( path.resolve('./views/home.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile( path.resolve('./views/login.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile( path.resolve('./views/register.html'));
+})
